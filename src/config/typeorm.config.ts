6cf3e5eda78dc9b18f984a1a1880cc7fbd77418a
Paras Kumar <paras.kumar@almond.solutions>
2025-06-19 12:18:08 +0530

    migration script added

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,22 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+    type: 'mysql',
+    host: configService.get('DB_HOST', 'localhost'),
+    port: configService.get<number>('DB_PORT', 3306),
+    username: configService.get('DB_USERNAME', 'root'),
+    password: configService.get('DB_PASSWORD', 'password'),
+    database: configService.get('DB_DATABASE', 'coupons_db'),
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+    synchronize: false,
+    logging: configService.get('NODE_ENV') === 'development',
+    charset: 'utf8mb4',
+    timezone: 'Z',
+});
